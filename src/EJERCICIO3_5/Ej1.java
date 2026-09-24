8ab2f884458aceed414d3c80ce99185716486531
juanmaca119-ai <juanmaca119@gmail.com>
2026-09-24 09:25:05 +0200

    Primer punto hecho. Creación de la carpeta

--- src/EJERCICIO3_5/Ej1.java
+++ src/EJERCICIO3_5/Ej1.java
@@ -1,7 +1,18 @@
 package EJERCICIO3_5;
 
+import java.io.File;
+
 public class Ej1 {
     static void main() {
         System.out.println("Evidencia de conexión con GitHub");
+
+        File carpeta = new File("copias");
+
+        if (carpeta.exists()) {
+            System.out.println("La carpeta ya existe");
+        } else {
+            carpeta.mkdir();
+            System.out.println("Se ha creado la carpeta");
+        }
     }
 }
